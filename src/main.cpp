36740f597b4e07a7096aa49355050ef7a28f4ada
mitsiu-carreno <mitsiu.carreno@gmail.com>
2021-06-20 11:22:56 -0500

    code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "thread.hpp"
-//#include "game_controller.hpp"
 #include <ncurses.h>
 
 
@@ -7,8 +6,6 @@ int main(){
   initscr();  // start curses mode
 
   thread::HandleThreads();
-  //game_controller::MainLoop();
-  //InputTest();
 
   endwin();   // end curses mode
   return 0;
--- src/thread.cpp
+++ src/thread.cpp
@@ -2,30 +2,8 @@
 #include "game_controller.hpp"
 #include "input_handler.hpp"
 #include <thread>
-#include <future>
-#include <mutex>
-#include <string>
 
 namespace thread{
-  /*
-  // As we have multiple threads executing at the same time, we 
-  // gotta handle the std::cout calls so messages don't get mixed up
-  void PrintSafe(std::string const &s){
-    // Our mutex will flag and therefore protect that each std::cout 
-    // call is handled one at the time, static is needed so that the 
-    // var prevail through several calls
-    static std::mutex my_mutex;
-
-    // Is a mutex wrapper that provides a mechanism for owning 
-    // a mutex for the duration of a scoped block
-    // at the end of the block lock_guard is destructed and
-    // the mutex is released
-    const std::lock_guard<std::mutex> l(my_mutex);
-    std::cout << s;
-    std::cout << std::flush>;
-  }
-  */
-
   void HandleThreads(){
 
     // Create a new thread that calls GetNewDirection()
